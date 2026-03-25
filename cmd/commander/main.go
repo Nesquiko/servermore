a8e912984a17518004ac02ec925099c057bc9b6e
Nesquiko <github@nesquiko.dev>
2026-03-25 15:14:27 +0100

    Test #3 of protecting main

--- cmd/commander/main.go
+++ cmd/commander/main.go
@@ -10,8 +10,8 @@ import (
 
 func main() {
 	config := commander.CommanderConfig{
-		AppName:         "commander",
 		Env:             "LOCAL",
+		AppName:         "commander",
 		Host:            "localhost",
 		HttpPort:        "42069",
 		GrpcPort:        "42070",
